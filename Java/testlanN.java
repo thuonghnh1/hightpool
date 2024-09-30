d9483e4a500ed6a4aecf0a0ff2aaf2587974f774
Dino <ngquangthang0003@gmail.com>
2024-09-30 13:33:23 +0700

    Deleta forder java

--- Java/testlanN.java
+++
@@ -1,9 +0,0 @@
-package Java;
-public class testlanN {
-
-    public static void main(String[] args) {
-        System.out.println("hhehe");
-        System.out.println("hehhe");
-        System.out.println("hehe");
-    }
-}
