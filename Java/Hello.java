ebc7742a56f4f26e31fbd1e06dca40d5e00562fa
Dino <nguyenquangthang0003@gmail.com>
2024-09-30 13:12:04 +0700

    lần N

--- Java/Hello.java
+++ Java/Hello.java
@@ -3,6 +3,8 @@ package Java;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello lần Nfff"); 
-
+        System.out.println("híc hic");
+        System.out.println("théng");
     }
+    
 }
