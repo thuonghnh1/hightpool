c820870cd6df224aa49341ddb27b240cdba2e480
Dino <ngquangthang0003@gmail.com>
2024-09-30 13:31:31 +0700

    lần NN

--- Java/testlanN.java
+++ Java/testlanN.java
@@ -4,5 +4,6 @@ public class testlanN {
     public static void main(String[] args) {
         System.out.println("hhehe");
         System.out.println("hehhe");
+        System.out.println("hehe");
     }
 }
